cadc47cd97f051bbb6c1652c8d36017062554f9c
Unknown <thibaut.laggoune@epitech.eu>
2018-03-23 18:25:54 +0100

    Select Menu
    + main menu button

---
+++ Volt/Assets/SelectLevel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevel : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Load1()
+    {
+        SceneManager.LoadScene("niveau1");
+    }
+
+    public void Load2()
+    {
+        SceneManager.LoadScene("niveau2");
+    }
+
+    public void LoadMm()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Load3()
+    {
+        SceneManager.LoadScene("niveau3");
+    }
+
+    public void LoadSelect()
+    {
+        SceneManager.LoadScene("SelectScene");
+    }
+}
