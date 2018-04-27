37b5acf7c58984009f51237c79654bc76d063b12
Unknown <remi.wickuler@epitech.eu>
2018-04-27 13:05:29 +0200

    new push

---
+++ Volt/Assets/scene/endGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class endGame : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player" && other.gameObject.transform.localScale.x < 0)
+		{
+			SceneManager.LoadScene("niveau4");
+		}
+	}
+}
